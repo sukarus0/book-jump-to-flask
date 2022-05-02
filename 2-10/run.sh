76d638a384a6c52d20dbdf83b71ef8213126791a
sukarus0 <sukarus@gamil.com>
2022-05-02 07:18:26 +0000

    update run.sh (multi package handling)

--- 2-10/run.sh
+++ 2-10/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # install python packages
-if [ -z "$(pip list | grep Flask-Migrate)" ]; then
-	pip install Flask-Migrate
-else
-	echo "Flask-Migrate already installed"
-fi
+required_pkg='Flask-Migrate Flask-WTF'
+pkg_list=($required_pkg)
+
+for package in ${pkg_list[@]};
+do
+	if [ -z "$(pip list | grep ${package})" ]; then
+		pip install ${package}
+	else
+		echo "${package} already installed"
+	fi
+done
 
 # run flask app
 FLASK_APP=pybo FLASK_ENV=development flask run --host=0.0.0.0 --port=5000
